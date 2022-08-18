087d9e205ed25c79b35cfb264138c6f5238a0335
niccololampa <niccololampa@gmail.com>
2022-08-18 18:11:09 +0800

    Fix errror "'React' refers to a UMD global, but the current file is a module"

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import jsonData from './data/characters.json'
@@ -6,13 +7,18 @@ const data: Character[] = jsonData as Character[]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
           Learn React
         </a>
       </header>

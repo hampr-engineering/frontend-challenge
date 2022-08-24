e81680c42dbb2bbeca36cd35f8abb6e303f8afc3
niccololampa <niccololampa@gmail.com>
2022-08-24 22:32:41 +0800

    Add helper function for accessing specific ability in character ability array

---
+++ src/helpers/AbilityScoreHelper.tsx
@@ -0,0 +1,11 @@
+import type { Character, CharacterAbility, CharacterTag } from '../types'
+const abilityScoreHelper = (abilities: CharacterAbility[], abilityFilter: string) => {
+  return abilities.find((ability: CharacterAbility) => {
+    if (ability.abilityName === abilityFilter) {
+      return true
+    }
+    return false
+  }).abilityScore
+}
+
+export default abilityScoreHelper

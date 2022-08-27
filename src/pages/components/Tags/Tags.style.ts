c70ad39bad90e04e0a59be9f783bdc2d7d0e5f38
narges safari <narges.safari.8631@gmail.com>
2022-08-27 22:39:17 +0430

    initialize tags

---
+++ src/pages/components/Tags/Tags.style.ts
@@ -0,0 +1,11 @@
+import { createUseStyles } from "react-jss";
+
+const useTagsStyle = createUseStyles({
+  tag: {
+    margin: 4,
+    cursor: "pointer",
+    fontSize: 18,
+  },
+});
+
+export default useTagsStyle;
---
+++ src/pages/components/Tags/Tags.tsx
@@ -0,0 +1,22 @@
+import { Chip, Box } from "@mui/material";
+import { tagArray } from "./Tags.const";
+import useTagsStyle from "./Tags.style";
+
+const Tags = () => {
+  const classes = useTagsStyle();
+
+  return (
+    <Box paddingX={4}>
+      {tagArray.map((tag: string) => (
+        <Chip
+          className={classes.tag}
+          color={"primary"}
+          label={tag}
+          variant={"outlined"}
+        />
+      ))}
+    </Box>
+  );
+};
+
+export default Tags;

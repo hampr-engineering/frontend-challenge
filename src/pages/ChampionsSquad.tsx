ba1f6955784c407a0ee43f853cb9b2a403c51dd3
narges safari <narges.safari.8631@gmail.com>
2022-08-28 13:22:36 +0430

    add components to champions root page

--- src/pages/ChampionsSquad.tsx
+++ src/pages/ChampionsSquad.tsx
@@ -1,10 +1,31 @@
-import type { Character } from "./ChampionsSquad.types";
-import charactersJson from "../assets/data/characters.json";
-
-const data: Character[] = charactersJson as Character[];
+import { Box, Avatar } from "@mui/material";
+import DragonAvatar from "../assets/images/Mortal-Kombat-Logo.png";
+import ChampionsBoard from "./components/ChampionsBoard";
+import Chip from "./components/Tags";
+import CharactersTable from "./components/CharactersTable";
 
 const ChampionsSquad = () => {
-  return <div>ChampionsSquad</div>;
+  return (
+    <Box height={"100vw"}>
+      <Box
+        height={76}
+        width={"100%"}
+        bgcolor={"black"}
+        display={"flex"}
+        position={"sticky"}
+        justifyContent={"center"}
+      >
+        <Avatar
+          src={DragonAvatar}
+          alt={"dragon-avatar"}
+          sx={{ height: 88, width: 88 }}
+        />
+      </Box>
+      <ChampionsBoard />
+      <Chip />
+      <CharactersTable />
+    </Box>
+  );
 };
 
 export default ChampionsSquad;

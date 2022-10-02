6476ab403575044153d1ddce428e2cac7741228f
Maisie <maisie.embleton@sharesight.co.nz>
2022-10-02 18:41:38 +0800

    Adding a styled Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import logo from '../../img/Mortal-Kombat-Logo.png';
+import { StyledImage } from './Image.styled';
+
+
+export const Logo: FC =() => <StyledImage src={logo} alt="Mortal Kombat Logo" /> 

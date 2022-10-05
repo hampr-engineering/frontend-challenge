9025609c047f1a41551165645bcc9908b5f8924b
Maisie <maisie.embleton@sharesight.co.nz>
2022-10-05 14:57:05 +0800

    Removing styled-components

--- src/components/Logo/Image.styled.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledImage = styled.img`
-    height: 90px;
-    margin-top: 32px;
-`;
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,6 +1,12 @@
+import { Box } from '@mui/material';
 import { FC } from 'react';
 import logo from '../../img/Mortal-Kombat-Logo.png';
-import { StyledImage } from './Image.styled';
 
-
-export const Logo: FC =() => <StyledImage src={logo} alt="Mortal Kombat Logo" /> 
+export const Logo: FC = () => (
+  <Box
+    component="img"
+    sx={{ height: '90px', marginTop: '32px' }}
+    src={logo}
+    alt="Mortal Kombat Logo"
+  />
+);
